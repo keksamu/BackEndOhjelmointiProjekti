eac46405a2221a1db3b65e6a1947b3421ae60866
bhq631@myy.haaga-helia.fi <bhq631@myy.haaga-helia.fi>
2025-11-03 14:15:43 +0200

    SpringBoot tehty

---
+++ nba-players/src/main/java/com/example/projekti/nba_players/NbaPlayersApplication.java
@@ -0,0 +1,13 @@
+package com.example.projekti.nba_players;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbaPlayersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NbaPlayersApplication.class, args);
+	}
+
+}
---
+++ nba-players/src/test/java/com/example/projekti/nba_players/NbaPlayersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projekti.nba_players;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbaPlayersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
