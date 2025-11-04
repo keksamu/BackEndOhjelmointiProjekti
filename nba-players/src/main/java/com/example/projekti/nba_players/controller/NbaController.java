be17e91e196626deec2fb0ad1e4d5b62307162ff
bhq631@myy.haaga-helia.fi <bhq631@myy.haaga-helia.fi>
2025-11-04 20:45:46 +0200

    Tehty templateja

--- nba-players/src/main/java/com/example/projekti/nba_players/controller/NbaController.java
+++ nba-players/src/main/java/com/example/projekti/nba_players/controller/NbaController.java
@@ -1,20 +1,32 @@
 package com.example.projekti.nba_players.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.GetMapping;
 
+import com.example.projekti.nba_players.model.PlayerRepository;
+import com.example.projekti.nba_players.model.PositionRepository;
 
 @Controller
 public class NbaController {
 
-    @RequestMapping("/main")
-    @ResponseBody
-    public String playerName() {
-        return "LeBron James";
-    }
-    
+    private final PlayerRepository playerRepository;
+    private final PositionRepository positionRepository;
 
+    public NbaController(PlayerRepository playerRepository, PositionRepository positionRepository) {
+        this.playerRepository = playerRepository;
+        this.positionRepository = positionRepository;
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @RequestMapping(value="/playerlist")
+    public String playerList(Model model) {
+        model.addAttribute("players", playerRepository.findAll());
+        return "playerlist";
+    }
 }
