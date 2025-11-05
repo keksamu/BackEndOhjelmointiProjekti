b700354b617cbc76dfdfc76482629136c198f60e
bhq631@myy.haaga-helia.fi <bhq631@myy.haaga-helia.fi>
2025-11-05 18:15:47 +0200

    Edit ja Delete

--- nba-players/src/main/java/com/example/projekti/nba_players/controller/NbaController.java
+++ nba-players/src/main/java/com/example/projekti/nba_players/controller/NbaController.java
@@ -1,9 +1,11 @@
 package com.example.projekti.nba_players.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.example.projekti.nba_players.model.Player;
 import com.example.projekti.nba_players.model.PlayerRepository;
@@ -44,6 +46,29 @@ public class NbaController {
     @PostMapping("/addplayer")
     public String savePlayer(Player player) {
         playerRepository.save(player);
+        return "redirect:/playerlist";
+    }
+
+    /*@PreAuthorize("hasAuthority('ADMIN')")*/
+    @GetMapping("/delete/{id}")
+    public String deletePlayer(@PathVariable("id") Long playerId) {
+        playerRepository.deleteById(playerId);
+        return "redirect:../playerlist";
+    }
+
+   /* @PreAuthorize("hasAuthority('ADMIN')") */
+    @GetMapping("/edit/{id}")
+    public String editPlayer(@PathVariable("id") Long playerId, Model model) {
+        Player player = playerRepository.findById(playerId).orElse(null);
+        model.addAttribute("player", player);
+        model.addAttribute("positions", positionRepository.findAll());
+        return "editplayer";
+    }
+
+    @PostMapping("/edit/{id}")
+    public String updatePlayer(@PathVariable("id") Long id, Player updatedPlayer) {
+        updatedPlayer.setId(id);
+        playerRepository.save(updatedPlayer);
         return "redirect:/playerlist";
     }
 
